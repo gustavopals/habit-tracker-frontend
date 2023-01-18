909ab9421d48e1b62809fcda2fff1c4090dc528c
Gustavo Pals <gustavoandrepals@gmail.com>
2023-01-18 10:26:06 -0300

    feat: created header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import "./styles/global.css";
-
-import logoImage from "./assets/logo.svg";
+import { Header } from "./components/Header";
 
 export function App() {
 	return (
 		<div className="w-screen h-screen flex justify-center items-center">
 			<div className="w-full max-w-5xl px-6 flex flex-col gap-16">
-				<img src={logoImage} alt="habits" />
+				<Header></Header>
 			</div>
 		</div>
 	);
