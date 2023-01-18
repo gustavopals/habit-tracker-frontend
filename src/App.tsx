444b13f4cc4a538ef874cfe668c45ee5ee6be71e
Gustavo Pals <gustavoandrepals@gmail.com>
2023-01-18 10:26:53 -0300

    feat: created header component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export function App() {
 	return (
 		<div className="w-screen h-screen flex justify-center items-center">
 			<div className="w-full max-w-5xl px-6 flex flex-col gap-16">
-				<Header></Header>
+				<Header />
 			</div>
 		</div>
 	);
